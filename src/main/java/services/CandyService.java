9178e8856a88d319ebd80674930e7131c8afce89
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-13 20:21:12 -0500

    Candy Services updated

--- src/main/java/services/CandyService.java
+++ src/main/java/services/CandyService.java
@@ -1,4 +1,19 @@
 package services;
 
+import models.Candy;
+
+import java.util.ArrayList;
+
 public class CandyService {
+    private static Integer nextId = 1;
+
+    private ArrayList<Candy> inventory = new ArrayList<>();
+
+    public Candy create(String name, String brand, Integer sku, Double weightInOunces, Integer qty, Double price) {
+        Candy createdCandy = new Candy(nextId++, name, brand, sku, weightInOunces, qty, price);
+
+        inventory.add(createdCandy);
+
+        return createdCandy;
+    }
 }
--- src/test/java/services/CandyServiceTest.java
+++ src/test/java/services/CandyServiceTest.java
@@ -1,4 +1,44 @@
 package services;
 
+import models.Candy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CandyServiceTest {
+    @Test
+    public void CandyServiceTest(){
+
+        // (1)
+        String expectedName = "Gobstoppers";
+        String expectedBrand = "Wonka";
+        Integer expectedSku = 1037177;
+        Double expectedWeightInOunces = 6.0;
+        Integer expectedQty = 6700;
+        Double expectedPrice = 1.19;
+
+        // (2)
+        CandyService CandyService = new CandyService();
+        Candy testCandy = CandyService.create(expectedName, expectedBrand,
+                expectedSku, expectedWeightInOunces, expectedQty, expectedPrice);
+
+        // (3)
+        Integer actualId = testCandy.getId();
+        String actualName = testCandy.getName();
+        String actualBrand = testCandy.getBrand();
+        Integer actualSku = testCandy.getSku();
+        Double actualWeightInOunces = testCandy.getWeightInOunces();
+        Integer actualQty = testCandy.getQty();
+        Double actualPrice = testCandy.getPrice();
+
+        // (4)
+        assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        assertEquals(expectedName, actualName);
+        assertEquals(expectedBrand, actualBrand);
+        assertEquals(expectedSku, actualSku);
+        assertEquals(expectedWeightInOunces, actualWeightInOunces);
+        assertEquals(expectedQty, actualQty);
+        assertEquals(expectedPrice, actualPrice);
+
+    }
 }
