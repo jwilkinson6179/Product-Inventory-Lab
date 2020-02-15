4705fa7ba16e61db7e51df90a0b828a5a0c5a38d
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-14 21:46:12 -0500

    First lab -- work in progress

--- src/test/java/services/CandyServiceTest.java
+++ src/test/java/services/CandyServiceTest.java
@@ -1,21 +1,26 @@
 package services;
 
 import models.Candy;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CandyServiceTest {
+
+    //@Before
+    // TODO: Learn @Before to properly configure this test file
+
     @Test
     public void CandyServiceTest(){
 
         // (1)
-        String expectedName = "Gobstoppers";
-        String expectedBrand = "Wonka";
-        Integer expectedSku = 1037177;
-        Double expectedWeightInOunces = 6.0;
-        Integer expectedQty = 6700;
-        Double expectedPrice = 1.19;
+        String expectedName = "Whatchamacallit";
+        String expectedBrand = "Nestle";
+        Integer expectedSku = 140955;
+        Double expectedWeightInOunces = 8.2;
+        Integer expectedQty = 112;
+        Double expectedPrice = 1.89;
 
         // (2)
         CandyService CandyService = new CandyService();
@@ -41,4 +46,72 @@ public class CandyServiceTest {
         assertEquals(expectedPrice, actualPrice);
 
     }
+
+    @Test
+    public void CandyServiceTestFind()
+    {
+        CandyService candyService = new CandyService();
+
+
+        candyService.create("Gobstoppers", "Wonka",
+                170655, 7.1, 19, 1.49);
+        Candy actual = candyService.create("Snickers", "Hershey",
+                551042, 4.9, 88, 0.89);
+        candyService.create("Big Red", "Wrigley",
+                667302, 4.1, 64, 0.39);
+        candyService.create("Reese's Peanut Butter Cups", "Hershey",
+                552301, 5.2, 4, 1.39);
+        Candy expected = candyService.find(2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CandyServiceTestFindTwo()
+    {
+        CandyService candyService = new CandyService();
+
+
+        candyService.create("Gobstoppers", "Wonka",
+                170655, 7.1, 19, 1.49);
+        candyService.create("Snickers", "Hershey",
+                551042, 4.9, 88, 0.89);
+        candyService.create("Big Red", "Wrigley",
+                667302, 4.1, 64, 0.39);
+        Candy actual = candyService.create("Reese's Peanut Butter Cups", "Hershey",
+                552301, 5.2, 4, 1.39);
+        Candy expected = candyService.find(4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CandyServiceTestFindAll()
+    {
+        CandyService CandyService = new CandyService();
+
+
+        Candy actualCandy0 = CandyService.create("Gobstoppers", "Wonka",
+                170655, 7.1, 19, 1.49);
+        Candy actualCandy1 = CandyService.create("Snickers", "Hershey",
+                551042, 4.9, 88, 0.89);
+        Candy actualCandy2 = CandyService.create("Big Red", "Wrigley",
+                667302, 4.1, 64, 0.39);
+        Candy actualCandy3 = CandyService.create("Reese's Peanut Butter Cups", "Hershey",
+                552301, 5.2, 4, 1.39);
+        Candy[] expected = { actualCandy0, actualCandy1, actualCandy2, actualCandy3 };
+        Candy[] actual = CandyService.findAll();
+
+        assertEquals(expected, actual);
+    }
+/*
+    @Test
+    public void CandyServiceTestDeleteFail()
+    {
+        delete(1);
+        findAll()
+    }
+
+    @Test
+*/
 }
